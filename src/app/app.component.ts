955d74a4c9c6b9ef5d7d56f05ec4c1d06a562bbe
Jishnu <jishnupunathil000@gmail.com>
2023-06-16 14:22:14 +0530

    team created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
   newMemb:string='';
   membList:string[]=[];
   errorMessage:string='';
+  numberOfTeams: '' | number = '';
+  teams:string[][]=[];
 
   onInput(member:string){
     this.newMemb=member;
+  }
+
+  onTeamInput(value:string){
+    this.numberOfTeams=Number(value);
   }
 
 
@@ -27,5 +32,34 @@ export class AppComponent {
     this.membList.push(this.newMemb);
     this.newMemb='';
   }
+
+  generateTeams(){
+    
+    if(!this.numberOfTeams || this.numberOfTeams <= 0){
+      this.errorMessage="invalid number of teams"
+      return
+  }
+  this.errorMessage=''
+  const allMembers=[...this.membList]
+
+  while(allMembers.length>0){
+    for(let i=0;i<this.numberOfTeams;i++){
+      const randomIndex=Math.floor(Math.random()*allMembers.length)
+  
+      const member=allMembers.splice(randomIndex,1)[0]
+      if(!member) break
+      if(this.teams[i]){
+        this.teams[i].push(member)
+      }else{
+        this.teams[i]=[member]
+      }
+  
+    }
+  }
+
+this.membList=[]
+this.numberOfTeams=''
+  
+}
 
 }
